c529db9350db87f9ca3fffb13e3f21ca5e4225f0
Vaughn Walters <vaughnwalters@gmail.com>
2017-02-16 11:58:47 -0600

    added firebase and initialized on client side

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common'
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+  // lifecycle method - automatically called once defined
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAqllTZZbpe2HvQVqI16rnNLyzXYElk37o',
+      authDomain: 'authentication-55dac.firebaseapp.com',
+      databaseURL: 'https://authentication-55dac.firebaseio.com',
+      storageBucket: 'authentication-55dac.appspot.com',
+      messagingSenderId: '1061684497943'
+    });
+  }
   render() {
     return (
       <View>
