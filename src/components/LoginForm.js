1507b8572f51e3ec8184666dc24b87ba69510586
Vaughn Walters <vaughnwalters@gmail.com>
2017-02-16 15:29:25 -0600

    adding styling to input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
   state = { text: '' }
@@ -9,10 +9,13 @@ class LoginForm extends Component {
     return (
       <Card>
         <CardSection />
-          <TextInput 
+          <Input 
+            label="Email"
             value={this.state.text}
+            // this function will be called when user types
+            // and when we call setState, the component immediately re-renders
+            // when a user enters text into the input, it has no idea what its value is, but the value is saved as this.state and then we tell the input that the text it contains will be this.state.text
             onChangeText={text => this.setState({ text })}
-            style={{ height: 20, width: 100 }} 
           />
         <CardSection />
 
