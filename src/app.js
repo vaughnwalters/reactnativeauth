8879a1c9d10ff1eaae6c464d4427903cc3a3512b
Vaughn Walters <vaughnwalters@gmail.com>
2017-02-17 14:45:44 -0600

    working on changing view when logged in or out

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: false };
+
   // lifecycle method - automatically called once defined
   componentWillMount() {
     firebase.initializeApp({
@@ -14,15 +16,38 @@ class App extends Component {
       storageBucket: 'authentication-55dac.appspot.com',
       messagingSenderId: '1061684497943'
     });
-  }
-  render() {
-    return (
-      <View>
-        <Header headerText="Authentication" />
-        <LoginForm />
-      </View>
-    );
+    
+    // this event handler handles signing in or signing out
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
 }
+
+
+renderContent() {
+  if (this.state.loggedIn) {
+    return (
+      <Button>
+        Log out
+      </Button>
+    );
+  }
+   return <LoginForm />;
+}
+
+render() {
+  return (
+    <View>
+      <Header headerText="Authentication" />
+      {this.renderContent()}
+    </View>
+  );
+}
+
 
 export default App;
